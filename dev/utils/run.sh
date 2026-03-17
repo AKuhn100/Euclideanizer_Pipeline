8cc5071cc8bfb90c623cea22f42e57a2734897bf
Adam Kuhn <adamkuhn100@gmail.com>
2026-03-16 20:39:13 -0500

    3-16

--- dev/utils/run.sh
+++ dev/utils/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=Euclideanizer_Pipeline
 #SBATCH --partition=commons
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=1024G
+#SBATCH --mem=256G
 #SBATCH --time=24:00:00
 #SBATCH --output=/scratch/amk19/slurm_outputs/%x.%j.out
 #SBATCH --error=/scratch/amk19/slurm_outputs/%x.%j.err
@@ -12,4 +12,4 @@ source /scratch/amk19/Euclideanizer/.venv/bin/activate
 
 module load GCCcore/13.3.0 FFmpeg/7.0.2
 
-python /scratch/amk19/Euclideanizer/Pipeline/run_hpo.py --config /scratch/amk19/Euclideanizer/Pipeline/dev/configs/hpo_config.yaml
+python /scratch/amk19/Euclideanizer/Pipeline/run.py --config /scratch/amk19/Euclideanizer/Pipeline/dev/configs/config_2.yaml
