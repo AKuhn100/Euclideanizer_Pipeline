28d5cdfac02c5afcf88e49aab8e5826897721162
Adam Kuhn <adamkuhn100@gmail.com>
2026-03-17 12:39:02 -0500

    Rerunning some configs with optimal pairwise allignment in coord clustering analysis

--- dev/utils/run.sh
+++ dev/utils/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=Euclideanizer_Pipeline
 #SBATCH --partition=commons
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:8
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=512G
+#SBATCH --mem=1024G
 #SBATCH --time=24:00:00
 #SBATCH --output=/scratch/amk19/slurm_outputs/%x.%j.out
 #SBATCH --error=/scratch/amk19/slurm_outputs/%x.%j.err
@@ -12,4 +12,4 @@ source /scratch/amk19/Euclideanizer/.venv/bin/activate
 
 module load GCCcore/13.3.0 FFmpeg/7.0.2
 
-python /scratch/amk19/Euclideanizer/Pipeline/run.py --config /scratch/amk19/Euclideanizer/Pipeline/dev/configs/config_21_gen_cap.yaml
+python /scratch/amk19/Euclideanizer/Pipeline/run_hpo.py --config /scratch/amk19/Euclideanizer/Pipeline/dev/configs/hpo_config.yaml
